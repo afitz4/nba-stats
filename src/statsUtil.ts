9bfad3db8ccd9ff2cbab25e767406658b4e866b8
Andrew Fitz <andrewfitz@Andrews-MacBook-Pro.local>
2023-04-17 01:07:02 -0400

    WIP

---
+++ src/statsUtil.ts
@@ -0,0 +1,7 @@
+import { Player } from "./models/Player";
+
+export function getInterestingPlayers(allPlayers: Player[]) {
+    allPlayers.forEach((player) => {
+        // filter them down
+    })
+}
